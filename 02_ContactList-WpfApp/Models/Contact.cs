4d1a92395c32ac63a8984b5be84b45839cd20da1
Andolo2 <andreasolofsson123@gmail.com>
2023-01-16 19:37:46 +0100

    Started on wpf application

---
+++ 02_ContactList-WpfApp/Models/Contact.cs
@@ -0,0 +1,21 @@
+﻿
+using _02_ContactList_WpfApp.IInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_ContactList_WpfApp.Models
+{
+    public class Contact : IContact
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get ; set ; } = string.Empty;
+        public string PhoneNumber { get ; set ; }
+        public string Email { get ; set ; } = string.Empty;
+        public string Adress { get ; set ; }
+        public string PostalCode { get ; set ; }
+        public string City { get ; set ; }
+    }
+}
---
+++ 02_ContactList-WpfApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_ContactList_WpfApp.ViewModels
+{
+    public partial class MainViewModel : ObservableObject // from CommunityToolKit.MVVM, update changes dynamicly
+    {
+        [ObservableProperty]
+        private ObservableObject currentViewModel;
+
+        public MainViewModel()
+        {
+            CurrentViewModel = new ContactsViewModel();
+        }
+    }
+}
